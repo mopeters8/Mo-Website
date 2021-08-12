df32606241e18fd939a93b9438967cfcf0965a4c
mopeters8 <mpeters4@dons.usfca.edu>
2021-08-12 15:42:55 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const session = require('express-session');
+const passport = require('passport');
+const discordStrategy = require('./strategies/discordstrategy');
+
+// Routes
+const authRoute = require('./routes/auth');
+
+app.use(session({
+    secret: 'some random secret',
+    cookie: {
+        maxAge: 60000 * 60 * 24
+    },
+    saveUninitialized: false
+}));
+
+
+// Passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middleware Routes
+app.use('/auth', authRoute)
+
+app.listen(PORT, () => {
+    console.log(`Now listening to requests on port ${PORT}`)
+});
---
+++ src/strategies/discordstrategy.js
@@ -0,0 +1,14 @@
+
+const DiscordStrategy = require('passport-discord').Strategy;
+const passport = require('passport');
+
+passport.use(new DiscordStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CLIENT_REDIRECT,
+    scope: ['identify', 'guilds', 'email']
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(profile.username);
+    console.log(profile.id);
+    console.log(profile.email)
+}));
