6c0ea3cc4996b52367a9c7d964aa690fa9ab1ee4
mopeters8 <mpeters4@dons.usfca.edu>
2021-08-16 14:58:36 -0700

    beginning construction on calendar.

--- src/models/DiscordUser.js
+++ src/models/DiscordUser.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     discordId: { type: String, required: true },
     username: { type: String, required: true },
-    useravatar: { type: String, required: true},
-    email: { type: String, required: true },
-    guilds: { type: Array, required: true }
+    useravatar: { type: String, required: false},
+    email: { type: String, required: false },
+    guilds: { type: Array, required: false }
 });
 
 const DiscordUser = module.exports = mongoose.model('User', UserSchema);
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -12,14 +12,38 @@ function isAuthorized(req, res, next) {
 }
 
 router.get('/', isAuthorized, (req, res) => { //NOT /DASHBOARD BECAUSE WE ALREADY REGISTERED / DASHBOARD.
+    var avatar;
+    if(req.user.useravatar == null) {
+        console.log("Warning: No Profile Picture for User, " + req.user.username);
+        avatar = "https://imgur.com/DeIq43J.png";
+    } else {
+        avatar = "https://cdn.discordapp.com/avatars/"+req.user.discordId+"/"+req.user.useravatar+".png";
+    }
     res.render('dashboard', {
         username: req.user.username,
-        useravatar: req.user.useravatar,
+        avatarsrc: avatar,
         discordId: req.user.discordId,
-        discordemail: req.user.discordemail,
+        discordemail: avatar,
         guilds: req.user.guilds
     });
 });
+
+router.get('/calendar', isAuthorized, (req, res) => {
+    var avatar;
+    if(req.user.useravatar == null) {
+        console.log("Warning: No Profile Picture for User, " + req.user.username);
+        avatar = "https://imgur.com/DeIq43J.png";
+    } else {
+        avatar = "https://cdn.discordapp.com/avatars/"+req.user.discordId+"/"+req.user.useravatar+".png";
+    }
+    res.render('friendcalendar', {
+        username: req.user.username,
+        avatarsrc: avatar,
+        discordId: req.user.discordId,
+        discordemail: avatar,
+        guilds: req.user.guilds
+    });
+})
 
 router.get('/settings', isAuthorized, (req, res) => { //NOT /DASHBOARD BECAUSE WE ALREADY REGISTERED / DASHBOARD.
     res.send("oh you logged in my boy")
